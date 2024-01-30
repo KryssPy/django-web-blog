de5651288fbf488039eea4ad732c273374c77681
KrysPy <krstnbanach@gmail.com>
2024-01-30 19:18:29 +0100

    wip

--- kryspy_app/settings.py
+++ kryspy_app/settings.py
@@ -12,11 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASE_URL = 'postgresql://postgres:1c-46-df4AaE5352Agg34CeE5G*5f*21@postgres.railway.internal:5432/railway'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,7 +25,7 @@ SECRET_KEY = 'django-insecure-^0-3g(7=tz71i2o!nup%j^c4-+s*r+sd^j6!!oy9xxcpj_y)*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-web-blog-production.up.railway.app']
 
 # Application definition
 
@@ -79,7 +77,14 @@ WSGI_APPLICATION = 'kryspy_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gGc-Cf3CAaA4g16a-E6a4EEDDDc2FBA4',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '25941',
+    }
 }
 
 
